86fecc503532a276a5ca9b357675d406853177ee
viochris <140993389+viochris@users.noreply.github.com>
2025-10-24 16:55:58 +0700

    Update used_func.py

--- used_func.py
+++ used_func.py
@@ -12,11 +12,17 @@ def get_top_n_words_en(corpus, n, ngram_range):
 
 
 def get_top_n_words_id(corpus, n, ngram_range):
-    vec = CountVectorizer(ngram_range=ngram_range, stop_words=stopwords.words('indonesian')).fit(corpus)
+    stopword = stopwords.words('indonesian')
+    if "luar" in stopword or "biasa" in stopword
+        stopword.remove(luar)
+        stopword.remove(biasa)
+    
+    vec = CountVectorizer(ngram_range=ngram_range, stop_words=stopword).fit(corpus)
     bag = vec.transform(corpus)
     sum_words = bag.sum(axis=0)
     words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
     return sorted(words_freq, key=lambda x: x[1], reverse=True)[:n]
 
 def convert_for_download(df):
+
     return df.to_csv(index=False).encode("utf-8")
